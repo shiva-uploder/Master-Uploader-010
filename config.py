d700473f03d6f73581e271482e13ebbf990ad97e
Rajesh R Mahar <136462044+jairamjiki@users.noreply.github.com>
2025-04-28 11:35:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ class Config(object):
     AUTH_USER = os.environ.get('AUTH_USERS', '7621154046').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
-    CREDIT = " 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
+    CREDIT = "[꧁ 𝐉𝐨𝐡𝐧 𝐖𝐢𝐜𝐤 ꧂](https://t.me/Dc5txt_bot)"
