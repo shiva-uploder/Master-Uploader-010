afe8841f60dd71e051e9429394ae7467ce3fcfb0
shiva-uploder <ps8546572@gmail.com>
2025-05-04 14:52:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH","626ed6dab78881858778d9663682962e")
     AUTH_USER = os.environ.get('AUTH_USERS', '2088265361').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
-    HOST = "https://drm-api-six.vercel.app"
+    HOST = "https://api.masterapi.tech"
     CREDIT = "[🅱🅴🅰🆂🆃 👑](https://t.me/chiru52)"
